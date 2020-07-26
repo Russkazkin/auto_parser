f3e3ef57c5e68b633decb2ac44df2531becde559
Russkazkin <ruslan@skazkin.su>
2020-07-26 12:36:09 +0300

    Add node_name field into categories table

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,10 +12,10 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
-            ['name' => 'Легковые автомобили', 'slug' => 'cars'],
-            ['name' => 'Грузовые и автобусы', 'slug' => 'trucks'],
-            ['name' => 'Коммерческий транспорт', 'slug' => 'commercial'],
-            ['name' => 'Мотоциклы', 'slug' => 'motorcycles'],
+            ['name' => 'Легковые автомобили', 'slug' => 'cars', 'node_name' => '#bmVendorTypesC0'],
+            ['name' => 'Грузовые и автобусы', 'slug' => 'trucks', 'node_name' => '#bmVendorTypesC1'],
+            ['name' => 'Коммерческий транспорт', 'slug' => 'commercial', 'node_name' => '#bmVendorTypesC2'],
+            ['name' => 'Мотоциклы', 'slug' => 'motorcycles', 'node_name' => '#bmVendorTypesC3'],
         ]);
     }
 }
