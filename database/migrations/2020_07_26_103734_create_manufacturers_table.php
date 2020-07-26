dfa5de0a83a37eda804904d4366615abbb0fd591
Russkazkin <ruslan@skazkin.su>
2020-07-26 13:37:58 +0300

    Generate Manufacturer model and migration

--- database/migrations/2020_07_26_103734_create_manufacturers_table.php
+++ database/migrations/2020_07_26_103734_create_manufacturers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCarsTable extends Migration
+class CreateManufacturersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCarsTable extends Migration
      */
     public function up()
     {
-        Schema::create('cars', function (Blueprint $table) {
+        Schema::create('manufacturers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateCarsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cars');
+        Schema::dropIfExists('manufacturers');
     }
 }
