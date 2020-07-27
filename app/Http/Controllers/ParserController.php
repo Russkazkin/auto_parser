4c7e9eb708e08983ae126a623b19d7d78417dbab
Russkazkin <ruslan@skazkin.su>
2020-07-27 07:06:19 +0300

    Complete cars parsing action

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -29,24 +29,29 @@ class ParserController extends Controller
 
     public function cars()
     {
-        $link = 'https://exist.ru/Catalog/Global/Cars/Ford?all=1';
-        $html = file_get_contents($link);
+        set_time_limit(3600);
+        $manufacturers = Manufacturer::all();
+        foreach ($manufacturers as $manufacturer)
+        {
+            $link = 'https://exist.ru' . $manufacturer->uri . '/?all=1';
+            $html = file_get_contents($link);
 
-        $crawler = new Crawler(null, $link);
-        $crawler->addHtmlContent($html);
-        $crawler->filter('.cell2')->each(function (Crawler $parentCrawler, $i){
-            $name_node = $parentCrawler->filter('.car-info__car-name > a');
-            $name = $name_node->text();
-            $uri = $name_node->attr('href');
-            $years_node = $parentCrawler->filter('.car-info__car-years');
-            $years = $years_node->text();
-            Car::create([
-                'name' => $name,
-                'years' => $years,
-                'uri' => $uri,
-                'manufacturer_id' => 1
-            ]);
-        });
+            $crawler = new Crawler(null, $link);
+            $crawler->addHtmlContent($html);
+            $crawler->filter('.cell2')->each(function (Crawler $parentCrawler) use ($manufacturer){
+                $name_node = $parentCrawler->filter('.car-info__car-name > a');
+                $name = $name_node->text();
+                $uri = $name_node->attr('href');
+                $years_node = $parentCrawler->filter('.car-info__car-years');
+                $years = $years_node->text();
+                Car::create([
+                    'name' => $name,
+                    'years' => $years,
+                    'uri' => $uri,
+                    'manufacturer_id' => $manufacturer->id,
+                ]);
+            });
+        }
         return view('parser.cars');
     }
 }
