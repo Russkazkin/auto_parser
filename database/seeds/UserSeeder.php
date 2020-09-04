5d205f1120531654cce5d438758f7a5656a59582
Russkazkin <ruslan@skazkin.su>
2020-09-04 11:22:03 +0300

    Add admin user seeding

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::where('email', 'ruslan@skazkin.su')->first();
+
+        if(!$user) {
+            User::create([
+                'name' => 'admin',
+                'email' => 'ruslan@skazkin.su',
+                'password' => '$2y$10$Hv7RJYtTrhrSTcgFVKcz0.aUfVWkQIBB13BpTbHTrknSkwgpQzvim',
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+    }
+}
